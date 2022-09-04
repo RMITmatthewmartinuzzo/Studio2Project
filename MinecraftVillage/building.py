52962af8146649e51e684b221a7bf0f799e67e84
RMITMatthewMartinuzzo <s3850470@student.rmit.edu.au>
2022-09-05 00:44:16 +1000

    create buildRoom function

--- MinecraftVillage/building.py
+++ MinecraftVillage/building.py
@@ -1,5 +1,6 @@
 from mcpi.minecraft import Minecraft
 from mcpi.minecraft import Block
+from mcpi.minecraft import Vec3
 from random import randint
 
 class Building():
@@ -18,7 +19,40 @@ class Building():
         self.build()
     
     def build(self):
-        buildingWidth, buildingLength, roomHeight = randint(10, 15), randint(10, 15), randint(3, 5)
+        #recursive function to split rooms into smaller rooms
+        def buildRooms(buildingWidth, buildingLength, roomHeight, startingLocation):
+            
+            # blockDoor = Block(64)
+            # self.mCon.setBlock(startingLocation.x + 1, startingLocation.y, startingLocation.z, blockDoor)
+            # self.mCon.setBlock(startingLocation.x + 1, startingLocation.y + 1, startingLocation.z, blockDoor)
+            
+            area = buildingWidth * buildingLength
+            areaRandomizer = randint(0, 100)
+            #splitting stops upon an area of 9 or less, or if the randomizer is less than 10
+            if area <= 9 or areaRandomizer < 10:
+                return
+            #choose either room to be divided in x or y direction
+            direction = 1
+            #this is in the x direction
+            if direction == 1:
+                #choose a random point to split the room
+                split = randint(1, buildingWidth - 1)
+                blockObj = Block(1, 0)
+                for y in range(roomHeight):
+                    for z in range(1, buildingLength - 1):
+                        self.mCon.setBlock(startingLocation.x + split, startingLocation.y + y, startingLocation.z + z, blockObj)
+                leftLocation = Vec3(startingLocation.x + 1, startingLocation.y, startingLocation.z)
+                rightLocation = Vec3(startingLocation.x + split + 1, startingLocation.y, startingLocation.z)
+                
+                buildRooms(split, buildingLength, roomHeight, leftLocation)
+                buildRooms(buildingWidth - split, buildingLength, roomHeight, rightLocation)
+            #else do it in the z direction
+            else:
+                pass
+        
+        
+        #width is in the x direction, length is in the z direction
+        buildingWidth, buildingLength, roomHeight = randint(50, 55), randint(50, 55), randint(3, 5)
         # do i want a map of my building? lets start with no map...
         print(buildingLength, buildingWidth)
         #build to the roof
@@ -33,6 +67,5 @@ class Building():
             for z in range(1, buildingLength - 1):
                 self.mCon.setBlock(self.startingLocation.x, self.startingLocation.y + y, self.startingLocation.z + z, blockObj)
                 self.mCon.setBlock(self.startingLocation.x + buildingWidth - 1, self.startingLocation.y + y, self.startingLocation.z + z, blockObj)
-        
-        def buildRooms():
-            pass
+
+        buildRooms(buildingWidth, buildingLength, roomHeight, self.startingLocation)
--- MinecraftVillage/main.py
+++ MinecraftVillage/main.py
@@ -1,5 +1,6 @@
 from mcpi.minecraft import Minecraft
 from mcpi.minecraft import Block
+from mcpi.minecraft import Vec3
 from building import Building
 from village import Village
 
