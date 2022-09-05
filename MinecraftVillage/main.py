690587f6577aada8e48d201426df763d75de52a6
GeordieEK <geordieek@gmail.com>
2022-09-05 16:35:11 +1000

    Fixed import statements to update new village class

--- MinecraftVillage/main.py
+++ MinecraftVillage/main.py
@@ -1,7 +1,12 @@
 from mcpi.minecraft import Minecraft
 from building import Building
 import village
+import decoration
 
 if __name__ == "__main__":
     # create minecraft connection
     mc = Minecraft.create()
+
+    player_pos = mc.player.getTilePos()
+
+    # fountain = decoration.Fountain(mc, *player_pos)
