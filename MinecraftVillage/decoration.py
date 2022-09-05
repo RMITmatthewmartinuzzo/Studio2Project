cc33ab1eb9846bf381764a6620321b6b1bcf95b4
GeordieEK <geordieek@gmail.com>
2022-09-05 11:16:35 +1000

    Refactoring code to snake_case.

--- MinecraftVillage/decoration.py
+++ MinecraftVillage/decoration.py
@@ -7,19 +7,3 @@ from building import Building
 def create_torch(location):
     mc = Minecraft.create()
     mc.setBlock(location, 1)
-    pass
-
-
-class Torch:
-    def __init__(self, location):
-        create_torch(location)
-
-
-class Lantern:
-    def __init__(self, location):
-        self.create_lantern(location)
-
-    def create_lantern(self, location):
-        mc = Minecraft.create()
-        mc.setBlock(location, 1)
-        pass
