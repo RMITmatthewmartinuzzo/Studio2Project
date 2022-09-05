d7956eb1f8d93eaaac5318478910147ad344c7e1
RMITMatthewMartinuzzo <s3850470@student.rmit.edu.au>
2022-09-05 13:59:54 +1000

    re-coding Building class

--- MinecraftVillage/main.py
+++ MinecraftVillage/main.py
@@ -6,9 +6,3 @@ from decoration import Torch
 if __name__ == "__main__":
     # create minecraft connection
     mc = Minecraft.create()
-
-    starting_position = mc.player.getTilePos()
-
-    # village = Village(starting_position)
-
-    building = Building(starting_position, "pos x", mc)
