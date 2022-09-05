cf2c9a72be3fa2121bca683f306369bf9f593b13
RMITMatthewMartinuzzo <s3850470@student.rmit.edu.au>
2022-09-05 13:47:27 +1000

    attempting to stop recursion when flatness is bad

--- MinecraftVillage/FlatLand.py
+++ MinecraftVillage/FlatLand.py
@@ -29,6 +29,8 @@ class FlatLand:
         
     # recursive function to help find flat land with depth first search
     def flat_land_helper(self, row, column, new_land, current_flatness):
+        if current_flatness <= 0:
+            return
         # we want to store current co-ordinates in new_land
         new_land.append((row, column))
         # mark current block as seen
@@ -38,15 +40,27 @@ class FlatLand:
         # check if we can go down
         if current_height == self.world_map[row + 1][column] and (row + 1, column) not in self.seen_blocks and row + 1 < len(self.world_map):
             self.flat_land_helper(row + 1, column, new_land, current_flatness)
+        else:
+            current_flatness = current_flatness - abs(current_height - self.world_map[row + 1][column])
 
         # check if we can go up
         if current_height == self.world_map[row - 1][column] and (row - 1, column) not in self.seen_blocks and row - 1 > 0:
             self.flat_land_helper(row - 1, column, new_land, current_flatness)
+        else:
+            current_flatness = current_flatness - abs(current_height - self.world_map[row - 1][column])
         
         # check if we can go right
         if current_height == self.world_map[row][column + 1] and (row, column + 1) not in self.seen_blocks and column + 1 < len(self.world_map[row]):
             self.flat_land_helper(row, column + 1, new_land, current_flatness)
+        else:
+            current_flatness = current_flatness - abs(current_height - self.world_map[row][column + 1])
             
         #check if we can go left
         if current_height == self.world_map[row][column - 1] and (row, column - 1) not in self.seen_blocks and column - 1 > 0:
             self.flat_land_helper(row, column - 1, new_land, current_flatness)
+        else:
+            current_flatness = current_flatness - abs(current_height - self.world_map[row][column - 1])
+            
+    
+# issues...
+# dont think current_flatness is working
